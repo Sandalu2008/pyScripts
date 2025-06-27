7cdaef5e3c7bd346a86538068de6b639cd518756
Pradeep Suranga <bapsuranga@gmail.com>
2025-06-27 02:36:56 -0700

    commit1

---
+++ date_time2.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+# Create a date object 
+my_date = datetime.datetime(2023, 10, 27)
+print(f"My date: {my_date}")
+
+#Create a time object (hour, minute, second, microsecond)
+my_time = datetime.time(14, 30, 45, 123456)
+print(f"My Time: {my_time}")
+
+# Create a datetime object
+my_datetime = datetime.datetime(2024, 7, 24, 9, 15, 0)
+print(f"My datetime: {my_datetime}")
